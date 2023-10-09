39e5b9b9065f16d22a6df2551315387214de4816
Nathan Cosgrove <ncosgrove272@gmail.com>
2023-10-08 21:58:39 -0500

    Created an array

---
+++ app.js
@@ -0,0 +1,7 @@
+// This is an example of a simple array // 
+
+const vegetables = ["potatoes", "onions", "tomatoes", "lettuce", "carrots", "broccoli", "cucumbers"];
+
+console.log (vegetables)
+
+// This is an example of an array method, specifically the "remove" method // 
