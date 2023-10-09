a33ff54296d317838426bca4e1f1eb22c0dbfa69
Nathan Cosgrove <ncosgrove272@gmail.com>
2023-10-08 22:20:20 -0500

    Created 2 simple array methods

--- app.js
+++ app.js
@@ -2,6 +2,25 @@
 
 const vegetables = ["potatoes", "onions", "tomatoes", "lettuce", "carrots", "broccoli", "cucumbers"];
 
-console.log (vegetables)
+console.log (vegetables);
 
-// This is an example of an array method, specifically the "remove" method // 
+// This is an example of an array method, specifically the "splice()" method // 
+
+console.log("\n");
+
+vegetables.splice(3, 3);
+console.log(vegetables);
+
+// Another example array method, specifically the "join()" method // 
+
+console.log("\n");
+
+const vegetables1 = ["potatoes", "onions", "tomatoes", "lettuce", "carrots", "broccoli", "cucumbers"];
+
+console.log(vegetables1.join(" & "));
+
+// This is an example of a an object // 
+
+console.log("\n")
+
+
